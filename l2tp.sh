4b58012a08b60824544f20515c2f17e854b21aed
zyhut200 <99744043+zyhut200@users.noreply.github.com>
2022-09-24 00:00:49 +0800

    Update l2tp.sh

--- l2tp.sh
+++ l2tp.sh
@@ -1,5 +1,5 @@
 yum install firewalld firewall-config -y
 yum install -y wget
-wget --no-check-certificate https://raw.githubusercontent.com/Sunny8886667/l2tp-server-dev/master/install.sh
+wget --no-check-certificate https://raw.githubusercontent.com/zyhut200/l2tp/main/l2tp.sh
 chmod a+x install.sh
 ./install.sh
